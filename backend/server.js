c0052c3dc6f378532ff551ece04a8837a51dd2aa
SarrahGandhi <thesarrahgandhi@gmail.com>
2025-06-16 20:53:29 -0400

    render origin for cors

--- backend/server.js
+++ backend/server.js
@@ -17,6 +17,7 @@ app.use(
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:3000",
+      "https://milestone-manager-react.vercel.app",
     ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
